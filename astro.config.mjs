19d8a2888e424b8de7be8f722f0931d6ddda28bb
Beknur Saktanbekov <ssookkraatt@gmail.com>
2025-06-03 05:39:38 +0600

    Changed main theming and fixed theme toggling

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  trailingSlash: 'never',
+  trailingSlash: "never",
   build: {
     format: "file",
   },
